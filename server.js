c791453cf7b45472d367b073b99197f4bc576ef1
Ignacio Kornzaft <ikornzaft@gmail.com>
2020-12-11 15:55:55 -0300

    third commit

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
 const expressLayouts = require('express-ejs-layouts');
+const bodyParser = require('body-parser');
 
 const indexRouter = require('./routes/index');
 const authorRouter = require('./routes/authors');
@@ -20,6 +21,7 @@ app.set('views', __dirname + '/views');
 app.set('layout', 'layouts/layout');
 app.use(expressLayouts);
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }));
 
 app.use('/', indexRouter);
 app.use('/authors', authorRouter);
