b02123e3eee2088fc5ddedb9752f6f1d2f55d9da
ikornzaft <ikornzaft@gmail.com>
2020-12-15 06:50:39 -0300

    second video commit

--- routes/authors.js
+++ routes/authors.js
@@ -4,8 +4,14 @@ const Author = require('../models/author');
 
 // All Authors Route
 
-router.get('/', (req, res) => {
-  res.render('authors/index');
+router.get('/', async (req, res) => {
+  try {
+    const authors = await Author.find({});
+    res.render('authors/index', { authors: authors });
+  } catch {
+    console.log('error')
+    res.redirect('/');
+  };
 })
 
 // New Author Route
@@ -14,21 +20,38 @@ router.get('/new', (req, res) => {
 })
 
 // Create Author Route
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
+  
   const author = new Author({
     name: req.body.name
   })
-  author.save((err, newAuthor) => {
-    if(err) {
-      res.render('authors/new'), {
+  try {
+    const newAuthor = await author.save();
+    // res.redirect(`authors/${newAuthor.id}`);
+    res.redirect(`authors`);
+  } catch {
+    console.error('Error');
+    res.render("authors/new", {
         author: author,
-        errorMessage: 'Error creating Author'
-      }
+        errorMessage: "Error Creating Author"
+    })
+  };
+
+  /* const author = new Author({
+    name: req.body.name
+  })
+  author.save((err, newAuthor) => {
+    if (err) {
+      let locals = {errorMessage: "Error Creating Author"};
+      res.render("authors/new", {
+          author: author,
+          locals: locals
+      })
     } else {
       // res.redirect(`authors/${newAuthor.id}`);
       res.redirect(`authors`);
     }
-  })
+  }) */
 })
 
 module.exports = router;
