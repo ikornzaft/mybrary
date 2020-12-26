5a09b001afec36887b6d5e71be26e87f05f0d975
ikornzaft <ikornzaft@gmail.com>
2020-12-26 10:25:21 -0300

    Filter by date

--- routes/books.js
+++ routes/books.js
@@ -17,8 +17,19 @@ const upload = multer({
 // All Books Route
 
 router.get('/', async (req, res) => {
+  let query = Book.find({});
+  if (req.query.title != null && req.query.title != '') {
+    query = query.regex('title', new RegExp(req.query.title, 'i'));
+  };
+  if (req.query.publishedBefore != null && req.query.publishedBefore != '') {
+    query = query.lte('publishDate', req.query.publishedBefore);
+  };
+  if (req.query.publishedAfter != null && req.query.publishedAfter != '') {
+    query = query.gte('publishDate', req.query.publishedAfter);
+  };
   try {
-    const books = await Book.find({});
+    const books = await query.exec();
+    console.log(books);
     res.render('books/index', {
       books: books,
       searchOptions: req.query
